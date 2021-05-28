7b27e8fe5813944f732eea234d749e68c9fc62f0
Oluwafemi <phemmylintry@gmail.com>
2021-05-28 18:39:00 +0100

    lead generation api

--- keel/api/urls.py
+++ keel/api/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url, include
 from django.urls import path
 
-from .v1.auth.router import urlpatterns as auth_url
+# from .v1.auth.router import urlpatterns as auth_url
+
+from .views import CustomerLeadView
 
 
 urlpatterns = [
-    path('v1/user/', include(auth_url)),
-   
+    # path('v1/user/', include(auth_url)),
+    path('leads/', CustomerLeadView.as_view(), name="leads")
 ]
