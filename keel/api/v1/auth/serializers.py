b84d87a76edaab4be0e4a49dbdea618d0713bb98
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-06-15 16:44:37 +0100

    fixes to login api

--- keel/api/v1/auth/serializers.py
+++ keel/api/v1/auth/serializers.py
@@ -29,11 +29,12 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
 class LoginSerializer(serializers.ModelSerializer):
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
-    tokens = serializers.CharField(read_only=True)
+    access_token = serializers.CharField(read_only=True)
+    refresh_token = serializers.CharField(read_only=True)
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'tokens')
+        fields = ('email', 'password', 'access_token', 'refresh_token')
 
     def validate(self, attrs):
         email = attrs.get('email', None)
@@ -52,10 +53,8 @@ class LoginSerializer(serializers.ModelSerializer):
 
         data = {
             "email" : user,
-            "tokens" : {
-                "refresh" : str(token),
-                "access" : str(token.access_token)
-            }
+            "access_token" : str(token.access_token),
+            "refresh_token" : str(token)
         }
 
         return data
