ed80e89dddcb3c09fb7d5a16c7b2a9b28654fc39
udsharma015 <udit.sharma@happay.in>
2021-06-27 16:40:52 +0530

    Added auth clase to Document API & exception handing for Doc not found error

--- keel/api/v1/document/views.py
+++ keel/api/v1/document/views.py
@@ -6,12 +6,15 @@ from rest_framework import mixins, viewsets, status
 from rest_framework.response import Response
 
 from keel.document.models import Documents
+from keel.authentication.backends import JWTAuthentication
 
 from .serializers import DocumentsSerializer
 
 
 
 class GetDocument(GenericViewSet):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = (IsAuthenticated,)
 
     def generate(self, request, format = 'json', **kwargs):
         
@@ -39,7 +42,14 @@ class GetDocument(GenericViewSet):
         # doc_serializer = DocumentsSerializer(doc)
         # response_data = doc_serializer.data
         # response["data"] = response_data
-        file_handle = doc.avatar.open()
+        try:
+            file_handle = doc.avatar.open()
+        except FileNotFoundError as e:
+            response['status'] = 1
+            response['message'] = "File Not Found"
+            resp_status = status.HTTP_400_BAD_REQUEST
+            return Response(response, status = resp_status)
+
         response = FileResponse(file_handle, content_type='whatever')
         response['Content-Length'] = doc.avatar.size
         response['Content-Disposition'] = 'attachment; filename="%s"' % doc.avatar.name
