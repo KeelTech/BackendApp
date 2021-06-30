d88719a9f9f84ddda21ff41abb88c27e726b11d7
udsharma015 <udit.sharma@happay.in>
2021-06-30 22:15:19 +0530

    Redo ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,7 +3,7 @@ import logging
 
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
-ALLOWED_HOSTS = ['getkeel.com','admin.keel.com','*']
+ALLOWED_HOSTS = ['getkeel.com','admin.keel.com']
 
 DATABASES['default']['ATOMIC_REQUESTS'] = True  # noqa F405
 # DATABASES['default']['CONN_MAX_AGE'] = env.int('CONN_MAX_AGE', default=60)
