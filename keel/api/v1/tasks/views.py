069af116e125bdee1dedc8c22d92213ff288368a
udsharma015 <udit.sharma@happay.in>
2021-07-14 22:47:55 +0530

    Task Comments Create and Delete API

--- keel/api/v1/tasks/views.py
+++ keel/api/v1/tasks/views.py
@@ -155,10 +155,9 @@ class CommentService(GenericViewSet):
             log_error("ERROR", "CommentService: delete delete_comments",str(user_id), err = str(e))
             response["message"] = "Comment Id does not exist"
             response["status"] = 1
-            resp_status = HTTP_STATUS.HTTP_500_INTERNAL_SERVER_ERROR
+            return Response(response, status = HTTP_STATUS.HTTP_500_INTERNAL_SERVER_ERROR)
 
         comment_obj.mark_delete()
-
         return Response(response, status = resp_status)
 
 
