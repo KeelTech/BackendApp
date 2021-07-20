741be513cc9c191b3248a5093f3943df848436ac
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-07-20 16:56:06 +0100

    fixes on case api

--- keel/api/v1/cases/router.py
+++ keel/api/v1/cases/router.py
@@ -4,5 +4,5 @@ from .views import FilterUserCases, FilterUserCasesDetails, CaseView
 urlpatterns = [
     path('create-cases', CaseView.as_view(), name="create-case"),
     path('list-cases', FilterUserCases.as_view(), name='list-cases'),
-    path('list-cases-details/<int:pk>', FilterUserCasesDetails.as_view({'get':'get_case'}), name='list-cases-details'),
+    path('list-cases-details/<str:case_id>', FilterUserCasesDetails.as_view({'get':'get_case'}), name='list-cases-details'),
 ]
