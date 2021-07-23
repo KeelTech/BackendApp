b1c7c8f168bbde4ac166c500bcf14d65c7d63b06
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-07-23 14:07:40 +0100

    custom admin for quotation and milestones

--- keel/quotations/admin.py
+++ keel/quotations/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from .models import Quotation, QuotationMilestone
 
-# Register your models here.
-admin.site.register(Quotation)
-admin.site.register(QuotationMilestone)
+class QuotationAdmin(admin.ModelAdmin):
+
+    list_display = ('q_id', 'user', 'plan', 'total_amount', 'status')
+    autocomplete_fields = ('user', 'plan')
+    search_fields = ('q_id', )
+    readonly_fields = ('deleted_at',)
+
+class QuotationMilestoneAdmin(admin.ModelAdmin):
+
+    list_display = ('qm_id', 'due_date', 'amount', 'quotation', 'status')
+    autocomplete_fields = ('quotation', )
+    readonly_fields = ('deleted_at',)
+
+admin.site.register(Quotation, QuotationAdmin)
+admin.site.register(QuotationMilestone, QuotationMilestoneAdmin)
