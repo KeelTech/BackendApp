8eb3a60e8d791fdebf5824d341873949e24380bb
udsharma015 <udit.sharma@happay.in>
2021-07-30 20:22:38 +0530

    max allowed limit - 200

--- keel/api/v1/chats/pagination.py
+++ keel/api/v1/chats/pagination.py
@@ -6,7 +6,7 @@ CHAT_PAGINATION_LIMIT = 200
 class ChatsPagination(LimitOffsetPagination):
 
 	default_limit = CHAT_PAGINATION_LIMIT
-	max_limit = 1000000
+	max_limit = 200
 	min_limit = 1
 	min_offset = 0
 	max_offset = 1000000
