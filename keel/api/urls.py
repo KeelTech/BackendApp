07a249759a7fb37e9138d310cc6debb0f38ab163
Prateek Mirdha <prateekmirdha@Prateeks-MacBook-Pro.local>
2021-08-01 17:31:21 +0530

    Refactoring and removing save token usage

--- keel/api/urls.py
+++ keel/api/urls.py
@@ -1,23 +1,8 @@
 from django.conf.urls import url, include
 from django.urls import path
 
-from .v1.auth.router import urlpatterns as auth_url
-from .v1.leads.router import urlpatterns as leads_url
-from .v1.eligibility_calculator.router import urlpatterns as eligibility_url
-from .v1.document.router import urlpatterns as documents_url
-from .v1.tasks.router import urlpatterns as tasks_url
-from .v1.chats.router import urlpatterns as chats_url
-from .v1.cases.router import urlpatterns as cases_url
-from .v1.plans.router import urlpatterns as plans_url
+from .v1.router import urlpatterns as v1_url
 
 urlpatterns = [
-
-    path('v1/user/', include(auth_url), name="authentication"),
-    path('v1/leads/', include(leads_url)),
-    path('v1/eligibility/', include(eligibility_url)),
-    path('v1/doc/', include(documents_url)),
-    path('v1/tasks/', include(tasks_url)),
-    path('v1/chats/', include(chats_url)),
-    path('v1/cases/', include(cases_url)),
-    path('v1/plans/', include(plans_url)),
+    path('v1/', include(v1_url)),
 ]
