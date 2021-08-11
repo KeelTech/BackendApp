3f33ad3e60d8e57dd9edcafa013be4f8d6fcfecc
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-08-11 12:47:52 +0100

    remove deleted at from admin

--- keel/Core/admin.py
+++ keel/Core/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Country, City
+from .models import Country, City, State
 # Register your models here.
 
-admin.site.register(Country)
-admin.site.register(City)
+class CountryAdmin(admin.ModelAdmin):
+    readonly_fields = ('deleted_at', )
+
+class StateAdmin(admin.ModelAdmin):
+    readonly_fields = ('deleted_at', )
+
+class CityAdmin(admin.ModelAdmin):
+    readonly_fields = ('deleted_at', )
+
+
+admin.site.register(Country, CountryAdmin)
+admin.site.register(State, StateAdmin)
+admin.site.register(City, CityAdmin)
