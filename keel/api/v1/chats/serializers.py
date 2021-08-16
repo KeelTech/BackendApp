18f448e380a931178eb6163c589db452afd075e3
udsharma015 <udit.sharma@happay.in>
2021-08-16 22:13:56 +0530

    Create ChatRoom during chats get/list API

--- keel/api/v1/chats/serializers.py
+++ keel/api/v1/chats/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from keel.chats.models import Chat 
+from keel.chats.models import Chat, ChatRoom 
 from keel.cases.models import Case
 
 class ChatCreateSerializer(serializers.ModelSerializer):
@@ -12,3 +12,16 @@ class ChatCreateSerializer(serializers.ModelSerializer):
 
         chat_obj = Chat.objects.create(**validated_data)
         return chat_obj
+
+class ChatRoomSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ChatRoom
+        fields = "__all__"
+
+    def create(self, validated_data):
+
+        chat_room_obj = ChatRoom.objects.create(**validated_data)
+        return chat_room_obj
+
+
