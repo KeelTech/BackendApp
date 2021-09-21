e367200e21ad1fa346d783b186170abc8a9c3bd3
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-09-21 11:25:16 +0100

    program field on cases

--- keel/api/v1/cases/router.py
+++ keel/api/v1/cases/router.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import FilterUserCases, FilterUserCasesDetails, CaseView
+from .views import (FilterUserCases, FilterUserCasesDetails, CaseView, 
+                    UpdateCaseProgramView)
 
 urlpatterns = [
     path('create-cases', CaseView.as_view(), name="create-case"),
     path('list-cases', FilterUserCases.as_view(), name='list-cases'),
+    path('list-case-programs', UpdateCaseProgramView.as_view({'get':'get_all_programs'}),  name='get-all-programs'),
+    path('update-program/<str:case_id>', UpdateCaseProgramView.as_view({'post':'update_program'}), name='update-program'),
     path('list-cases-details/<str:case_id>', FilterUserCasesDetails.as_view({'get':'get_case'}), name='list-cases-details'),
 ]
--- keel/cases/admin.py
+++ keel/cases/admin.py
@@ -1,15 +1,15 @@
 from django.contrib import admin
 # import autocomplete_all as admin
 from django.contrib.auth import get_user_model
-from .models import Case
+from .models import Case, Program
 
 User = get_user_model()
 
 class CaseAdmin(admin.ModelAdmin):
-    list_display = ('case_id', 'user', 'agent', 'is_active')
+    list_display = ('case_id', 'user', 'agent', 'program', 'is_active')
     ordering = ('case_id',)
     search_fields = ['case_id']
-    autocomplete_fields = ['user', 'agent']
+    autocomplete_fields = ['user', 'agent', 'account_manager']
     readonly_fields=('deleted_at', 'case_id', 'display_id')
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
@@ -17,6 +17,12 @@ class CaseAdmin(admin.ModelAdmin):
             kwargs["queryset"] = User.objects.filter(user_type=User.CUSTOMER)
         if db_field.name == "agent":
             kwargs["queryset"] = User.objects.filter(user_type=User.RCIC)
+        if db_field.name == "account_manager":
+            kwargs["queryset"] = User.objects.filter(user_type=User.ACCOUNT_MANAGER)
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
+class ProgramAdmin(admin.ModelAdmin):
+    readonly_fields = ['deleted_at']
+
 admin.site.register(Case, CaseAdmin)
+admin.site.register(Program, ProgramAdmin)
