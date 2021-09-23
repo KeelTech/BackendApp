34a8f6265af50b52e10cbf295c461d68b8b4311d
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-09-23 12:45:45 +0100

    plan check list

--- keel/api/v1/plans/serializers.py
+++ keel/api/v1/plans/serializers.py
@@ -3,8 +3,17 @@ from keel.plans.models import Plan
 
 
 class PlanSerializers(serializers.ModelSerializer):
+    check_list = serializers.SerializerMethodField()
 
     class Meta:
         model = Plan
         fields = ('id', 'title', 'description', 'price', 'discount', 'currency', 'country_iso',
-                    'sgst', 'cgst', 'is_active')
+                    'sgst', 'cgst', 'check_list', 'is_active')
+    
+    def get_check_list(self, obj):
+        check_list = obj.check_list
+        enum_list = list()
+        keys = ['text', 'isChecked']
+        for item in check_list:
+            enum_list.append(dict(zip(keys, item.split('|'))))
+        return enum_list
