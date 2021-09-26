d8ff7e9cbc97511df12af81be2adf7a651ccec6a
MPratik <prateekmirdha@Prateeks-MacBook-Pro.local>
2021-09-26 12:36:44 +0530

    CORS error fix for payment api

--- keel/api/v1/payment/router.py
+++ keel/api/v1/payment/router.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from django.conf.urls import include
 
-from .stripe.router import urlpatterns as stripe_urls
-from .views import PaymentTransactionViewSet
+from keel.api.v1.stripe.router import urlpatterns as stripe_urls
+from .views import PaymentTransactionViewSet, OrderViewSet
 
 urlpatterns = [
     path("stripe/", include(stripe_urls)),
-    path("user/transactions/pending", PaymentTransactionViewSet.as_view({"get": "pending_transactions"}))
+    path("user/transactions/pending", PaymentTransactionViewSet.as_view({"get": "pending_transactions"})),
+    path("order/create", OrderViewSet.as_view({"post": "create"})),
 ]
--- keel/api/v1/payment/stripe/router.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import OrderViewSet, WebHookViewSet
-
-urlpatterns = [
-    path("create/order/", OrderViewSet.as_view({"post": "create"})),
-    path("webhook/", WebHookViewSet.as_view({"post": "process_event"})),
-]
