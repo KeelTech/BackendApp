aea46b75ebff15b1986540e3765c67814d0ce849
MPratik <prateekmirdha@Prateeks-MacBook-Pro.local>
2021-09-26 13:28:24 +0530

    Stripe payment seprate url

--- keel/api/v1/payment/router.py
+++ keel/api/v1/payment/router.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from django.conf.urls import include
 
-from keel.api.v1.stripe.router import urlpatterns as stripe_urls
 from .views import PaymentTransactionViewSet, OrderViewSet
 
 urlpatterns = [
-    path("stripe/", include(stripe_urls)),
-    path("user/transactions/pending", PaymentTransactionViewSet.as_view({"get": "pending_transactions"})),
+    path("transactions/pending", PaymentTransactionViewSet.as_view({"get": "pending_transactions"})),
     path("order/create", OrderViewSet.as_view({"post": "create"})),
 ]
