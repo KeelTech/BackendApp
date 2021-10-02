265f3faa3f8035cf1eae6b705607417cc6b6dbe3
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-10-02 17:26:11 +0100

    platform components

--- keel/plans/admin.py
+++ keel/plans/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from keel.Core.admin import CustomBaseModelAdmin
-from .models import Plan, Vendor, Service, ServicesPlans
+from .models import Plan, Vendor, Service, ServicesPlans, PlanPlatformComponents
 # Register your models here.
+
+class PlanPlatformInline(admin.TabularInline):
+    model = PlanPlatformComponents
 
 class PlanAdmin(admin.ModelAdmin):
     list_display = ('title', 'price', 'currency')
     search_fields = ('title', )
     readonly_fields=('deleted_at',)
+    inlines = [PlanPlatformInline]
 
 class VendorAdmin(admin.ModelAdmin):
     search_fields = ['name']
