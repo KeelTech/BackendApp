695274acc756b59c26a00561ef78ea634958667f
MPratik <prateekmirdha@Prateeks-MacBook-Pro.local>
2021-10-05 21:27:30 +0530

    Case id query in schedule details bug fix

--- keel/cases/implementation/case_util_helper.py
+++ keel/cases/implementation/case_util_helper.py
@@ -1,3 +1,4 @@
+from keel.cases.models import Case
 
 class CaseUtilHelper(object):
 
@@ -13,3 +14,10 @@ class CaseUtilHelper(object):
             "agent_id": case_model_obj.agent.pk,
             "account_manager_id": case_model_obj.account_manager_id,
         }
+
+    def get_case_id_from_user_model_obj(self, user_model_objs):
+        case_model_objs = Case.objects.filter(user__in=user_model_objs)
+        customer_case_id_map = {}
+        for case_obj in case_model_objs:
+            customer_case_id_map[case_obj.user.pk] = case_obj.pk
+        return customer_case_id_map
