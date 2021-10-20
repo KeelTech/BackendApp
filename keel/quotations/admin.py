ea6be2f57740a685e7f0d8d07614ebfadc3dec20
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-10-20 15:54:22 +0100

    Revert "Merge branch 'intent' into master"
    This reverts commit 9df2ec19ba0a5c597bc28bb4ac73d2d38a356e0b, reversing
    changes made to c13ebe8eca59c0a246002aa46efb8c04e6e91a87.

--- keel/quotations/admin.py
+++ keel/quotations/admin.py
@@ -1,10 +1,10 @@
 import logging
 
 from django.contrib import admin
+from keel.Core.err_log import logging_format
+from keel.Core.constants import LOGGER_CRITICAL_SEVERITY
 from keel.cases.models import Case
 from keel.Core.admin import CustomBaseModelAdmin
-from keel.Core.constants import LOGGER_CRITICAL_SEVERITY
-from keel.Core.err_log import logging_format
 from keel.payment.implementation.pay_manager import (
     PaymentManager, StructNewPaymentDetailArgs)
 from keel.payment.models import Order
@@ -18,7 +18,7 @@ PAYMENT_CLIENT_TYPE = Order.PAYMENT_CLIENT_STRIPE
 class QuotationMilestoneLine(admin.TabularInline):
     model = QuotationMilestone
     extra = 0
-    readonly_fields = ('deleted_at', )
+    readonly_fields = ('qm_id', )
 
 class QuotationAdmin(admin.ModelAdmin):
 
@@ -28,36 +28,33 @@ class QuotationAdmin(admin.ModelAdmin):
     readonly_fields = ('deleted_at', 'q_id')
     inlines = [QuotationMilestoneLine]
 
-    def save_formset(self, request, form, formset, change):
-        instances = formset.save(commit=False)
-        for instance in instances:
-            customer = instance.quotation.user
-            payment_manager = PaymentManager()
-            order_items = {
-                "quotation_milestone":[str(instance.pk)],
-                "plan": [],
-                "service": []
-            }
-            try:
-                payment_details = payment_manager.generate_payment_details(
-                    StructNewPaymentDetailArgs(customer_id=customer.pk, customer_currency="usd",
-                                            initiator_id=request.user.pk, payment_client_type=PAYMENT_CLIENT_TYPE,
-                                            case_id=None),
-                    order_items
-                )
-            except ValueError as err:
-                logger.error(logging_format(LOGGER_CRITICAL_SEVERITY, "QuotationAdmin.save_formset",
-                                "", description=str(err)))
-            except Exception as err:
-                logger.error(logging_format(LOGGER_CRITICAL_SEVERITY, "QuotationAdmin.save_formset",
-                                "", description=str(err)))
-        return super().save_formset(request, form, formset, change)
 
 class QuotationMilestioneAdmin(CustomBaseModelAdmin):
-
-    list_display = ('qm_id', 'amount', 'status', 'due_date')
-    readonly_fields = ('qm_id', 'deleted_at')
-
+    
+    def save_model(self, request, obj, form, change):
+        instance = super().save_model(request, obj, form, change)
+        
+        customer = obj.quotation.user
+        payment_manager = PaymentManager()
+        order_items = {
+            "quotation_milestone":[str(obj.pk)],
+            "plan": [],
+            "service": []
+        }
+        try:
+            payment_details = payment_manager.generate_payment_details(
+                StructNewPaymentDetailArgs(customer_id=customer.pk, customer_currency="usd",
+                                           initiator_id=request.user.pk, payment_client_type=PAYMENT_CLIENT_TYPE,
+                                           case_id=None),
+                order_items
+            )
+        except ValueError as err:
+            logger.error(logging_format(LOGGER_CRITICAL_SEVERITY, "QuotationMilestioneAdmin.save_model",
+                            "", description=str(err)))
+        except Exception as err:
+            logger.error(logging_format(LOGGER_CRITICAL_SEVERITY, "QuotationMilestioneAdmin.save_model",
+                            "", description=str(err)))
+        return instance
 
 admin.site.register(Quotation, QuotationAdmin)
 admin.site.register(QuotationMilestone, QuotationMilestioneAdmin)
