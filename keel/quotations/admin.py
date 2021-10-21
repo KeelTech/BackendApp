974603a32769612ddf1c9ab0d863ec9996c33102
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-10-21 08:16:23 +0100

    intent

--- keel/quotations/admin.py
+++ keel/quotations/admin.py
@@ -1,10 +1,24 @@
+import logging
+
 from django.contrib import admin
+from keel.cases.models import Case
 from keel.Core.admin import CustomBaseModelAdmin
+from keel.Core.constants import LOGGER_CRITICAL_SEVERITY
+from keel.Core.err_log import logging_format
+from keel.payment.implementation.pay_manager import (
+    PaymentManager, StructNewPaymentDetailArgs)
+from keel.payment.models import Order
+
 from .models import Quotation, QuotationMilestone
+
+logger = logging.getLogger('app-logger')
+
+PAYMENT_CLIENT_TYPE = Order.PAYMENT_CLIENT_STRIPE
+
 class QuotationMilestoneLine(admin.TabularInline):
     model = QuotationMilestone
-    max_num = 0
-    readonly_fields = ('qm_id', )
+    extra = 0
+    readonly_fields = ('deleted_at', )
 
 class QuotationAdmin(admin.ModelAdmin):
 
@@ -14,9 +28,36 @@ class QuotationAdmin(admin.ModelAdmin):
     readonly_fields = ('deleted_at', 'q_id')
     inlines = [QuotationMilestoneLine]
 
+    def save_formset(self, request, form, formset, change):
+        instances = formset.save(commit=False)
+        for instance in instances:
+            customer = instance.quotation.user
+            payment_manager = PaymentManager()
+            order_items = {
+                "quotation_milestone":[str(instance.pk)],
+                "plan": [],
+                "service": []
+            }
+            try:
+                payment_details = payment_manager.generate_payment_details(
+                    StructNewPaymentDetailArgs(customer_id=customer.pk, customer_currency="usd",
+                                            initiator_id=request.user.pk, payment_client_type=PAYMENT_CLIENT_TYPE,
+                                            case_id=None),
+                    order_items
+                )
+            except ValueError as err:
+                logger.error(logging_format(LOGGER_CRITICAL_SEVERITY, "QuotationAdmin.save_formset",
+                                "", description=str(err)))
+            except Exception as err:
+                logger.error(logging_format(LOGGER_CRITICAL_SEVERITY, "QuotationAdmin.save_formset",
+                                "", description=str(err)))
+        return super().save_formset(request, form, formset, change)
 
 class QuotationMilestioneAdmin(CustomBaseModelAdmin):
-    pass
+
+    list_display = ('qm_id', 'amount', 'status', 'due_date')
+    readonly_fields = ('qm_id', 'deleted_at')
+
 
 admin.site.register(Quotation, QuotationAdmin)
 admin.site.register(QuotationMilestone, QuotationMilestioneAdmin)
--- keel/quotations/models.py
+++ keel/quotations/models.py
@@ -1,7 +1,10 @@
 import uuid
 
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from keel.authentication.models import User
+from keel.cases.models import Case
 from keel.Core.models import SoftDeleteModel, TimeStampedModel
 from keel.plans.models import Plan
 
@@ -21,22 +24,39 @@ class Quotation(TimeStampedModel, SoftDeleteModel):
     )
 
     q_id = models.CharField(max_length=255, primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.deletion.DO_NOTHING, 
-                                    related_name = "user_quotaions", blank = True, null = True ) 
+    user = models.ForeignKey(User, on_delete=models.deletion.DO_NOTHING,
+                             related_name="user_quotations", blank=True, null=True)
+    case = models.ForeignKey(Case, on_delete=models.deletion.DO_NOTHING,
+                             related_name="case_quotations", blank=True, null=True)
     account_manager_id = models.IntegerField(null=True, blank=True)
-    plan = models.ForeignKey(Plan, on_delete = models.deletion.DO_NOTHING,
-                                    related_name = "plan_quotations")
-    total_amount = models.DecimalField(max_digits = 12, decimal_places=2)
-    status = models.PositiveSmallIntegerField(choices=QUO_STATUS_TYPE_CHOICES, 
-                                        verbose_name="quo_status", default=CREATED)
+    plan = models.ForeignKey(Plan, on_delete=models.deletion.DO_NOTHING,
+                             related_name="plan_quotations")
+    total_amount = models.DecimalField(max_digits=12, decimal_places=2)
+    status = models.PositiveSmallIntegerField(choices=QUO_STATUS_TYPE_CHOICES,
+                                              verbose_name="quo_status", default=CREATED)
 
     def save(self, *args, **kwargs):
         if not self.q_id:
             self.q_id = uuid.uuid4()
+        self.check_amount()
         super().save(*args, **kwargs)
+
+    def check_amount(self):
+        instances = self.milestones_quote.all()
+        amount = []
+        for instance in instances:
+            amount.append(int(instance.amount))
+        total_amount = sum(amount)
+        print(total_amount, self.total_amount)
+        if total_amount > self.total_amount:
+            raise ValidationError(_("Amount hihger than total amounts"))
+
 
     def get_currency(self):
         return self.plan.get_currency()
+
+    def get_case(self):
+        return self.case
 
 
 class QuotationMilestone(TimeStampedModel, SoftDeleteModel):
@@ -48,14 +68,14 @@ class QuotationMilestone(TimeStampedModel, SoftDeleteModel):
         (PAID, 'Paid'),
         (UNPAID, 'Unpaid'),
     )
-    qm_id = models.CharField(max_length=255, primary_key=True)
+    qm_id = models.CharField(max_length=255, primary_key=True, default=uuid.uuid4)
     due_date = models.DateTimeField(null=True, blank=True)
-    amount = models.DecimalField(max_digits = 12, decimal_places=2)
-    status = models.PositiveSmallIntegerField(choices=QUO_MILESTONES_STATUS_TYPE_CHOICES, 
-                                                verbose_name="quo_milestone_status", default=UNPAID)
-    description = models.TextField(null=True,blank=True)
-    quotation = models.ForeignKey(Quotation, on_delete = models.deletion.DO_NOTHING, 
-                                    related_name = "milestones_quote")
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    status = models.PositiveSmallIntegerField(choices=QUO_MILESTONES_STATUS_TYPE_CHOICES,
+                                              verbose_name="quo_milestone_status", default=UNPAID)
+    description = models.TextField(null=True, blank=True)
+    quotation = models.ForeignKey(Quotation, on_delete=models.deletion.DO_NOTHING,
+                                  related_name="milestones_quote")
 
     def get_plan(self):
         return self.quotation.plan
@@ -69,3 +89,15 @@ class QuotationMilestone(TimeStampedModel, SoftDeleteModel):
     def get_currency(self):
         return self.quotation.get_currency()
 
+    def get_case(self):
+        return self.quotation.get_case()
+
+    def update_case(self, case_model_obj):
+        quotation = Quotation.objects.select_for_update().get(pk=self.quotation.pk)
+        if not quotation.case:
+            quotation.case = case_model_obj
+            quotation.save()
+
+    def complete_payment(self):
+        self.status = self.PAID
+        self.save()
