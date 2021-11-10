33f4624446296efd2e141bdcef57cf9b540c0790
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-11-10 12:04:18 +0100

    custom message queue

--- keel/Core/tasks.py
+++ keel/Core/tasks.py
@@ -1,5 +1,44 @@
-from time import sleep
-from celery import shared_task, bootsteps
-from kombu import Queue, Consumer, Exchange
- 
-# 
+from config.celery import app
+import kombu
+from celery import bootsteps
+
+
+# setting publisher
+with app.pool.acquire(block=True) as conn:
+    exchange = kombu.Exchange(
+        name='myexchange',
+        type='direct',
+        durable=True,
+        channel=conn,
+    )
+    exchange.declare()
+
+    queue = kombu.Queue(
+        name='myqueue',
+        exchange=exchange,
+        routing_key='mykey',
+        channel=conn,
+        message_ttl=600,
+        queue_arguments={
+            'x-queue-type': 'classic'
+        },
+        durable=True
+    )
+    queue.declare()
+
+
+# setting consumer
+class MyConsumerStep(bootsteps.ConsumerStep):
+
+    def get_consumers(self, channel):
+        return [kombu.Consumer(channel,
+                               queues=[queue],
+                               callbacks=[self.handle_message],
+                               accept=['json'])]
+
+    def handle_message(self, body, message):
+        print('Received message: {0!r}'.format(body))
+        message.ack()
+
+
+app.steps['consumer'].add(MyConsumerStep)
