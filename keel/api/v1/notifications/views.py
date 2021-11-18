6b7386698125735fab9021496d1a3f116ef6c86c
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-11-18 10:50:25 +0100

    update seen col on notification

--- keel/api/v1/notifications/views.py
+++ keel/api/v1/notifications/views.py
@@ -20,10 +20,11 @@ class NotificationViews(GenericViewSet):
 
         try:
             user_case = request.user.users_cases.get(user=request.user)
-            queryset = InAppNotification.objects.filter(case_id=user_case).exclude(user_id=request.user)
+            queryset = InAppNotification.objects.filter(case_id=user_case, seen=False).exclude(user_id=request.user)
         except ObjectDoesNotExist as err:
             log_error(LOGGER_LOW_SEVERITY, "NotificationViews:get_notifications", request.user.id,
                                 description="Failed to get case for user")
+            response['status'] = 0
             response['message'] = str(err)
             return Response(response, status=status.HTTP_404_NOT_FOUND)
         except Exception as err:
@@ -34,4 +35,26 @@ class NotificationViews(GenericViewSet):
         
         serializer = self.serializer_class(queryset, many=True).data
         response['data'] = serializer
+        return Response(response, status=status.HTTP_200_OK)
+    
+    def mark_notification_read(self, request, pk):
+        response = {'status':1, 'message':'Successfully marked notification as read'}
+
+        try:
+            notification = InAppNotification.objects.get(id=pk)
+        except ObjectDoesNotExist as err:
+            log_error(LOGGER_LOW_SEVERITY, "NotificationViews:mark_notification_read", request.user.id,
+                                description="Failed to get notification")
+            response['status'] = 0
+            response['message'] = str(err)
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+        except Exception as err:
+            log_error(LOGGER_LOW_SEVERITY, "NotificationViews:mark_notification_read", request.user.id,
+                                description="An error occured")
+            response['message'] = str(err)
+            return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        notification.seen = True
+        notification.save()
+
         return Response(response, status=status.HTTP_200_OK)
