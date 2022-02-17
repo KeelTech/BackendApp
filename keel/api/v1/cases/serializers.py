41472b6a45727a14d110a9cdb028767018742028
Oluwafemi Adenuga <phemmylintry@gmail.com>
2022-02-17 09:41:53 +0100

    fix: number of unread chats

--- keel/api/v1/cases/serializers.py
+++ keel/api/v1/cases/serializers.py
@@ -48,7 +48,8 @@ class CasesSerializer(serializers.ModelSerializer):
         return ""
 
     def get_action_items(self, obj):
-        number_of_unread_messages = UnreadChats.user_unread_messages(obj)
+        unread = UnreadChats(obj)
+        number_of_unread_messages = unread.user_unread_messages()
         in_review_tasks = number_of_tasks_per_status(obj)["in_review_tasks"]
         return number_of_unread_messages + in_review_tasks
 
