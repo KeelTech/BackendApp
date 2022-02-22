1f8064cb9f28ae7f861395e8c2aec10f179d0cc2
Oluwafemi Adenuga <phemmylintry@gmail.com>
2022-02-22 13:12:21 +0100

    feat: make field read only after save

--- keel/records/admin.py
+++ keel/records/admin.py
@@ -17,6 +17,12 @@ class SalesUserAdmin(CustomBaseModelAdmin):
     list_filter = ("plan", "created_by")
     readonly_fields = ("deleted_at", "created_by")
 
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return self.readonly_fields + ("plan", "email", "agent")
+        else:
+            return self.readonly_fields
+
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "user":
             kwargs["queryset"] = User.objects.filter(user_type=User.CUSTOMER)
