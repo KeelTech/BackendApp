156c285b4bac3a458de3d0de72ed357e95382c65
ronit.dhingra <ronit.dhingra@flipkart.com>
2022-03-17 16:16:02 +0530

    fixed

--- keel/document/admin.py
+++ keel/document/admin.py
@@ -5,15 +5,17 @@ from .models import DocumentType, Documents, PublicDocuments
 # Register your models here.
 admin.site.register(Documents, CustomBaseModelAdmin)
 admin.site.register(DocumentType, CustomBaseModelAdmin)
-admin.site.register(PublicDocuments, PublicDocumentAdmin)
+
 
 
 class PublicDocumentAdmin(CustomBaseModelAdmin):
 	list_display = ('original_name', 'file_link')
 	
 	def file_link(self, obj):
-	 	if obj and obj.url:
-        	return obj.url
-    	return ""
+		if obj and obj.url:
+			return obj.url
+		return ""
 
-    file_link.allow_tags = True
+	# file_link.allow_tags = True
+
+admin.site.register(PublicDocuments, PublicDocumentAdmin)
