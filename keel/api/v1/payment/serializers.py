5654eb9138fd06e240193b6c6820d5c1155fa58f
Oluwafemi Adenuga <phemmylintry@gmail.com>
2022-04-13 07:15:10 +0100

    feat: user order details

---
+++ keel/api/v1/payment/serializers.py
@@ -0,0 +1,8 @@
+from keel.payment.models import UserOrderDetails
+from rest_framework import serializers
+
+
+class UserOrderDetailsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserOrderDetails
+        fields = "__all__"
