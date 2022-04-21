0769a16a3242ea9aaef86e18ced04e955403079e
Oluwafemi Adenuga <phemmylintry@gmail.com>
2022-04-21 09:34:04 +0100

    refactor: order id

--- keel/api/v1/payment/razor_payment.py
+++ keel/api/v1/payment/razor_payment.py
@@ -43,7 +43,7 @@ class RazorPay(object):
                 "",
                 description=str(err),
             )
-            resp['error'] = str(err)
+            resp["error"] = str(err)
         return resp
 
     def capture_payment(self, payment_id):
