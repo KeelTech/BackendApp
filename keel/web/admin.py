a2994b24f26391f019b4a122fe8b3a2091b12493
Oluwafemi Adenuga <phemmylintry@gmail.com>
2022-04-25 06:08:10 +0100

    feat: email template model

--- keel/web/admin.py
+++ keel/web/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
 from keel.Core.admin import CustomBaseModelAdmin
-from .models import Web, HomeLeads, WebsiteContactData, WebsiteComponents
+
+from .models import (
+    EmailTemplateModel,
+    HomeLeads,
+    Web,
+    WebsiteComponents,
+    WebsiteContactData,
+)
+
 
 class WebsiteComponenetsAdmin(CustomBaseModelAdmin):
-    list_display = ('title', 'component_name', )
+    list_display = (
+        "title",
+        "component_name",
+    )
 
 
 admin.site.register(Web)
 admin.site.register(HomeLeads)
 admin.site.register(WebsiteContactData)
 admin.site.register(WebsiteComponents, WebsiteComponenetsAdmin)
+admin.site.register(EmailTemplateModel)
